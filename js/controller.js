6da5e7fb08f221d159a4ee0b9a1e2798bf2839a8
Nam Wook Kim <namwkim85@gmail.com>
2015-06-16 20:34:27 -0700

    update

--- js/controller.js
+++ js/controller.js
@@ -1,9 +1,9 @@
 var massvis = angular.module('massvis', []);
 
 massvis.controller('mainCtrl', ['$scope',  function ($scope) {
-	$scope.projectTitle 		= "MASSVIS:"
-	$scope.projectSummary		= "Grumpy wizards make toxic brew for the evil Queen and Jack. \
-	One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed \
-	in his bed into a horrible vermin."
+	$scope.title 		= "Title"
+	$scope.subtitle		= "Subtitle"
 
+	$scope.aboutTitle = "aboutTitle"
+	$scope.projectDesc = "projectDesc"
 }]);
